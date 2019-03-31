19823482e1b2f87a9d9e243bb4f649a7991becee
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-03-31 06:41:30 +0530

    Readme

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
+var isLocal = true;
+var database = (isLocal) ? "mongodb://127.0.0.1/cs" : "mongodb+srv://root:mongodb@hereisdx-khs4b.mongodb.net/cs?retryWrites=true"; // Server
+var googleClient = (isLocal) ? "120289353582-qrn3fenvf6pkq0s8hfhk8002fju02g4g.apps.googleusercontent.com" : "860179520621-ajigborrngka61ggo7ueikn54hi3kuq1.apps.googleusercontent.com";//Server
+
 var vars = {
-    // database = 'mongodb://127.0.0.1/cs', //Local
-    database:"mongodb+srv://root:mongodb@hereisdx-khs4b.mongodb.net/cs?retryWrites=true", // Server
-    
-
-
-    // googleClient:"120289353582-qrn3fenvf6pkq0s8hfhk8002fju02g4g.apps.googleusercontent.com", //Local
-    googleClient:"860179520621-ajigborrngka61ggo7ueikn54hi3kuq1.apps.googleusercontent.com", //Server
-    googleSecret:""
+    database: database,
+    googleClient: googleClient,
+    googleSecret: ""
 }
+console.log(vars.database)
 module.exports = vars
